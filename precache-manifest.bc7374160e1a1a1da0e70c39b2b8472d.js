b4d38d02e708e1b6be44c1ab3b528bbcfdedb6f9
ruqia bibi <bcs.f17.m.024@gmail.com>
2020-11-17 16:17:22 +0500

    olx app

---
+++ precache-manifest.bc7374160e1a1a1da0e70c39b2b8472d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1e76275a338f537cd9afddd1d65d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1ffa4988a4f312e522e",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cf16abddc8883efd68ba",
+    "url": "/static/css/main.27fde1ed.chunk.css"
+  },
+  {
+    "revision": "d1ffa4988a4f312e522e",
+    "url": "/static/js/2.88077ef8.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/static/js/2.88077ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf16abddc8883efd68ba",
+    "url": "/static/js/main.1144d441.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "ec4f02e35a33d418552b8d3ad7c944a3",
+    "url": "/static/media/avatar.ec4f02e3.png"
+  },
+  {
+    "revision": "638181100538123713772ceb95a45777",
+    "url": "/static/media/bg.63818110.jpg"
+  },
+  {
+    "revision": "27b40c8c43cb66ec62f66db8508faf16",
+    "url": "/static/media/heart.27b40c8c.svg"
+  },
+  {
+    "revision": "9269fa80007b5efcb745b00505f80fe4",
+    "url": "/static/media/olxx.9269fa80.jpg"
+  },
+  {
+    "revision": "4cb8cbc478714518f696fe164d5f8157",
+    "url": "/static/media/phone-app.4cb8cbc4.png"
+  }
+]);
